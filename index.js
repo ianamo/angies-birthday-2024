e2178c248c3edb0d92a45c900e591dbae668be67
Ian Mosley <iananmo86@gmail.com>
2024-05-13 14:47:24 -0500

    Made mobile friendly

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function toggleHide() {
  text = document.getElementById('pic-text');
  text.classList.toggle('hidden'); 
+ pic = document.getElementById('pic');
+ pic.classList.toggle('dark')
+ pic.classList.toggle('darker');
 }
